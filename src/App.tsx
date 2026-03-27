3894cd48729969ac7f75ab52a3a5d6164ea876d0
steiusido <allysahipolito06@gmail.com>
2026-03-27 11:25:02 +0800

    made the site a standalone MFE website

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,6 @@
 import { Routes, Route, Navigate } from "react-router-dom";
-import DepartmentSelect from "./Pages/DepartmentSelect";
-import LandingAdminPage from "./Pages/LandingAdminPage";
-import CEAdminPage from "./Pages/admin/CEAdmin";
-import CPEAdminPage from "./Pages/admin/CPEAdmin";
-import ECEAdminPage from "./Pages/admin/ECEAdmin";
-import EEAdminPage from "./Pages/admin/EEAdmin";
-import IEAdminPage from "./Pages/admin/IEAdmin";
-import MEAdminPage from "./Pages/admin/MEAdmin";
-import MEEAdminPage from "./Pages/admin/MEEAdmin";
-import MFEAdminPage from "./Pages/admin/MFEAdmin";
-import LandingPage from "./Pages/LandingPage";
-import CEPage from "./Pages/departments/CE";
-import CPEPage from "./Pages/departments/CPE";
-import ECEPage from "./Pages/departments/ECE";
-import EEPage from "./Pages/departments/EE";
-import IEPage from "./Pages/departments/IE";
-import MEPage from "./Pages/departments/ME";
-import MEExcellencePage from "./Pages/departments/MEExcellence";
-import MEEPage from "./Pages/departments/MEE";
 import MFEPage from "./Pages/departments/MFE";
+import MFEAdminPage from "./Pages/admin/MFEAdmin";
 import ScrollToTop from "./components/ScrollToTop";
 
 export default function App() {
@@ -27,26 +9,18 @@ export default function App() {
       <ScrollToTop />
 
       <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/admin" element={<LandingAdminPage />} />
-        <Route path="/departments" element={<DepartmentSelect />} />
-        <Route path="/dept/CE" element={<CEPage />} />
-        <Route path="/dept/CPE" element={<CPEPage />} />
-        <Route path="/dept/ECE" element={<ECEPage />} />
-        <Route path="/dept/EE" element={<EEPage />} />
-        <Route path="/dept/IE" element={<IEPage />} />
-        <Route path="/dept/ME" element={<MEPage />} />
-        <Route path="/dept/ME/excellence" element={<MEExcellencePage />} />
-        <Route path="/dept/MEE" element={<MEEPage />} />
-        <Route path="/dept/MFE" element={<MFEPage />} />
-        <Route path="/dept/CE/admin" element={<CEAdminPage />} />
-        <Route path="/dept/CPE/admin" element={<CPEAdminPage />} />
-        <Route path="/dept/ECE/admin" element={<ECEAdminPage />} />
-        <Route path="/dept/EE/admin" element={<EEAdminPage />} />
-        <Route path="/dept/IE/admin" element={<IEAdminPage />} />
-        <Route path="/dept/ME/admin" element={<MEAdminPage />} />
-        <Route path="/dept/MEE/admin" element={<MEEAdminPage />} />
-        <Route path="/dept/MFE/admin" element={<MFEAdminPage />} />
+        {/* The root URL now loads the MFE Page directly */}
+        <Route path="/" element={<MFEPage />} />
+        
+        {/* The main admin route now goes straight to the MFE Admin */}
+        <Route path="/admin" element={<MFEAdminPage />} />
+        
+        {/* Optional: Keep this if you want explicit /dept/MFE to still work, 
+            though it's redundant since / is already MFE */}
+        <Route path="/dept/MFE" element={<Navigate to="/" replace />} />
+        <Route path="/dept/MFE/admin" element={<Navigate to="/admin" replace />} />
+
+        {/* Catch-all: Redirects any unknown URLs back to the MFE home page */}
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </>
